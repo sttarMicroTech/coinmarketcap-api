7d389c97553b748409c1f58dd889ec6ddb5a9591
victorratts13 <victor.ratts13@gmail.com>
2022-01-12 00:44:45 -0300

    create header controller

---
+++ controllers/headers.controllers.js
@@ -0,0 +1,19 @@
+
+export default headers = {
+     accept: 'application/json, text/plain, */*',
+     //'accept-encoding': 'gzip, deflate, br',
+     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+     //'content-length': 2,
+     'content-type': 'application/json',
+     //'fvideo-id': '32877c7528719138961e6555e740fd6bcdb284e4',
+     origin: 'https://coinmarketcap.com',
+     referer: 'https://coinmarketcap.com/',
+     //'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
+     //'sec-ch-ua-mobile': '?1',
+     'sec-ch-ua-platform': '"Android"',
+     'sec-fetch-dest': 'empty',
+     'sec-fetch-mode': 'cors',
+     'sec-fetch-site': 'same-site',
+     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36',
+     //'x-request-id': 'df106f54-1769-4622-ac50-2a6f919fb7c4'
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import api from "./api/rest.api.js";
+import headersControllers from "./controllers/headers.controllers.js";
 
 class marketcap {
      constructor(config = {
@@ -19,26 +20,7 @@ class marketcap {
 
      async category() {
           try {
-               var coins = await api.post(`/${this.config.pathModel}/${this.config.apiVersion}/calendar/category`, {}, {
-                    headers: {
-                         accept: 'application/json, text/plain, */*',
-                         //'accept-encoding': 'gzip, deflate, br',
-                         'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-                         //'content-length': 2,
-                         'content-type': 'application/json',
-                         //'fvideo-id': '32877c7528719138961e6555e740fd6bcdb284e4',
-                         origin: 'https://coinmarketcap.com',
-                         referer: 'https://coinmarketcap.com/',
-                         //'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"',
-                         //'sec-ch-ua-mobile': '?1',
-                         'sec-ch-ua-platform': '"Android"',
-                         'sec-fetch-dest': 'empty',
-                         'sec-fetch-mode': 'cors',
-                         'sec-fetch-site': 'same-site',
-                         'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36',
-                         //'x-request-id': 'df106f54-1769-4622-ac50-2a6f919fb7c4'
-                    }
-               });
+               var coins = await api.post(`/${this.config.pathModel}/${this.config.apiVersion}/calendar/category`, {}, {headers: headersControllers});
                return coins.data;
           } catch (error) {
                return error;
